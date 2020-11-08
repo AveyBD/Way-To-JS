ccbca6393abe6d3dab0d9f66284e76d12adce228
Debashish Das <talk@ddeb.me>
2020-11-08 15:55:42 +0600

    Added String Functions

--- 2.Day 2/6.String.js
+++ 2.Day 2/6.String.js
@@ -1,3 +1,9 @@
 let country = "Bangladesh";
+let who = ", We Love"
 console.log(country.length); // will print the length of the var
 console.log(country.charAt(3)); // Will print the 3rd char
+console.log(country.toUpperCase()); // Will uppercase the var
+console.log(country.toLowerCase()); // You know what it will do
+console.log(country.concat(who))
+// Slice Function
+console.log("We speak " + country.slice(0, 6));
