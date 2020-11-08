5d614dc4de8caf8c31d1e992cffa3830c0b0e2dd
Debashish Das <talk@ddeb.me>
2020-11-08 19:23:03 +0600

    Operator and Simple Calculator project

---
+++ 2.Day 2/8.Arithmetic and Assignment operator.js
@@ -0,0 +1,19 @@
+/*
+Arithmetic Operator
++ Add
+- Sub
+* Multiply
+/ Div
+% Modulus / Reminder
+** Power / Exponent
+-- (-1 / Decrement )
+++ ( +1 / Increment )
+ */
+
+/*
+Assignment Operator
+=, += , -=, *=, /= , %=, **=
+ */
+let x = 3;
+console.log(x += 7); // x = x+ 7
+
---
+++ 2.Day 2/9.Calculator Project.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Enter the fist number");
+num1 = parseInt(num1);
+let num2 = prompt("Enter the second number");
+num2 = parseInt(num2);
+document.write("The Sum is: " + (num1 + num2 ) + "<br>");
+document.write("The Sub is: " + (num1 - num2 ) + "<br>");
+document.write("The multi is: " + (num1 * num2 ) + "<br>");
+document.write("The div is: " + (num1 / num2 ) + "<br>");
